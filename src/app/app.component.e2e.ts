8d926c34731e97fb6d8285e8d4c4b9929ad2838a
Stevens Garcia <smalmsteeng@gmail.com>
2018-07-17 13:53:15 -0500

    implemented protractor tests

--- src/app/app.component.e2e.ts
+++ src/app/app.component.e2e.ts
@@ -1,11 +1,22 @@
 import {} from 'jasmine';
+import {browser, element, by, By, $, $$} from 'protractor';
 
 describe('AppComponent Tests', () => {
+  const todoListItems = element.all(by.css('li'));
   beforeEach(() => {
     browser.get('');
   });
   it('Browser should have a defined title', () => {
     expect(browser.getTitle()).toEqual('Angular Protractor');
+  });
+  it('Should get the number of items as defined in item object', () => {
+    expect(todoListItems.count()).toBe(3);
+  });
+  it('Should get the first item text as defined', () => {
+    expect(todoListItems.first().getText()).toEqual('test');
+  });
+  it('Should get the last item text as defined', () => {
+    expect(todoListItems.last().getText()).toEqual('refactor');
   });
 
 });
