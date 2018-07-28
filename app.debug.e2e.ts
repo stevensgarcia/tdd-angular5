6c58f868d080a89d2045ecfa0bebd39115a4534e
Stevens Garcia <smalmsteeng@gmail.com>
2018-07-28 14:32:05 -0500

    added id element

--- app.debug.e2e.ts
+++ app.debug.e2e.ts
@@ -2,11 +2,10 @@ import {browser, element, by, By} from 'protractor';
 
 describe('AppComponent Tests', () => {
   beforeEach(() => {
-    browser.get('/');
+    browser.get('');
   });
 
   it('Test spec for debug and play', () => {
-    browser.debugger();
     expect<any>(element(by.id('my_id')).getText()).toEqual('my text');
   });
 });
